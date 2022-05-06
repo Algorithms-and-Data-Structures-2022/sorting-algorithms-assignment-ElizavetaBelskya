a374190485d27effcd31d483f2fde1cc711968f8
Елизавета Бельская <nefrf@mail.ru>
2022-05-06 22:35:43 +0300

    Merge sort updated

--- main.cpp
+++ main.cpp
@@ -15,14 +15,18 @@ using namespace assignment;
 
 int main() {
 
-  std::vector<int> arr = {-39, -79, 66, 34, 8};
-  InsertionSort sort;
+  std::vector<int> arr = {-39, -79, 66, 34, 8, 90, -4, 12, 99, -600};
+  MergeSort sort;
   sort.Sort(arr);
   std::cout << arr[0] << endl;
   std::cout << arr[1] << endl;
   std::cout << arr[2] << endl;
   std::cout << arr[3] << endl;
   std::cout << arr[4] << endl;
+  std::cout << arr[5] << endl;
+  std::cout << arr[6] << endl;
+  std::cout << arr[7] << endl;
+  std::cout << arr[8] << endl;
 
 
   return 0;
